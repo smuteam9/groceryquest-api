1e250f1ea11f522bbdb5e12e0a7b0154205baf93
Justin Ledford <justinledford@gmail.com>
2016-11-06 13:55:09 -0600

    add item to list endpoint

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+import cgi
 import os
 
 app = Flask(__name__)
@@ -33,11 +35,45 @@ def autocomplete(text):
 
 @app.route('/api/lists')
 def get_lists():
-    results = {}
+    #TODO: get user id from request
     current_user = User.query.filter_by(id=1).first()
+
+    results = {}
     results['lists'] = [l.dict() for l in current_user.lists]
 
     return jsonify(**results)
+
+
+@app.route('/api/additem', methods=['POST'])
+def add_item_to_list():
+    """
+    Add item to grocery list,
+    expecting the following params from POST request:
+
+    user_id
+    list_id
+    position
+    product_id (optional)
+    name
+    """
+    #TODO: get user id from request
+    #TODO: assert parameters
+
+    params = {k: str(v) for k, v in request.get_json().items()}
+    params = {k: cgi.escape(v) for k, v in params.items()}
+    print(params)
+
+    grocery_list = List.query.filter_by(id=params['list_id'], user_id=1).first()
+
+    product_id = params.get('product_id', None)
+
+    item = ListItem(product_id, grocery_list.id,
+                    params['position'], params['name'])
+
+    db.session.add(item)
+    db.session.commit()
+
+    return "item added"
 
 
 if __name__ == '__main__':
